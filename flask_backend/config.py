bac20591c7fd249d8762087c36cba9b9ffe6d946
austinsarwar <austin_sarwar@yahoo.com>
2024-10-29 18:52:49 -0400

    update jaws db

--- flask_backend/config.py
+++ flask_backend/config.py
@@ -5,5 +5,5 @@ class Config:
     # Use an environment variable for the secret key or generate one if not set
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)  # Generates a random key
     # Use an environment variable for the database URI
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('JAWS_DB') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
