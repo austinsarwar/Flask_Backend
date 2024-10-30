c5091913958ebad85dc10b2aa514b5fbfe778006
austinsarwar <austin_sarwar@yahoo.com>
2024-10-29 20:34:13 -0400

    fix auth/routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,9 +1,9 @@
-from flask import request, jsonify
+from flask import Blueprint, request, jsonify
+from flask_login import login_user, logout_user, login_required, current_user
 from app import db, bcrypt
 from app.models import User
-from app.auth import auth_bp
-from flask_login import login_user, logout_user, login_required, current_user
-from flask import Blueprint
+
+auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/register', methods=['POST'])
 def register():
