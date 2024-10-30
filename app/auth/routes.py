0377008765d8aa8f0c67999f49a303e33a850725
austinsarwar <austin_sarwar@yahoo.com>
2024-10-29 20:30:10 -0400

    Fix Blueprint import in authentication routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -3,7 +3,7 @@ from app import db, bcrypt
 from app.models import User
 from app.auth import auth_bp
 from flask_login import login_user, logout_user, login_required, current_user
-
+from flask import Blueprint
 
 @auth_bp.route('/register', methods=['POST'])
 def register():
